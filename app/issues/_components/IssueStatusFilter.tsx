2bcffc155efbcb758a30d671b3534d1c25509e0e
Mansoor Hussain <qaisrani07@gmail.com>
2024-02-06 12:38:52 +0500

    Build the layout of the pagination component

--- app/issues/_components/IssueStatusFilter.tsx
+++ app/issues/_components/IssueStatusFilter.tsx
@@ -2,10 +2,11 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   const statuses: { label: string; value?: Status }[] = [
     { label: "All" },
@@ -15,15 +16,24 @@ const IssueStatusFilter = () => {
   ];
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || "all"}
       onValueChange={(status) => {
-        const query = status !== "unset" ? `?status=${status}` : "";
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+
+        const query = params.size ? "?" + params.toString() : "";
         router.push(`/issues${query}`);
       }}
     >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         {statuses.map((status) => (
-          <Select.Item key={status.value} value={status.value || "unset"}>
+          <Select.Item
+            key={status.value || "all"}
+            value={status.value || "all"}
+          >
             {status.label}
           </Select.Item>
         ))}
