8b51b5e75ca8d5691f6f420629bcdc429eba1c5c
Mansoor Hussain <qaisrani07@gmail.com>
2024-02-07 13:24:36 +0500

    Refactor: Extract IssueTable

--- app/components/IssueActions.tsx
+++ app/components/IssueActions.tsx
@@ -4,7 +4,7 @@ import IssueStatusFilter from "../issues/_components/IssueStatusFilter";
 
 const IssueActions = () => {
   return (
-    <Flex mb="5" justify="between">
+    <Flex justify="between">
       <IssueStatusFilter />
       <Button>
         <Link href="/issues/new">Create Issue</Link>
